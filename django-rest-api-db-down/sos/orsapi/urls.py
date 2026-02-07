a531b1413cecf3de32321202c14dafb08716c0be
SawanPanwar <skpanwar25@gmail.com>
2026-02-08 02:40:26 +0530

    done

--- django-rest-api-db-down/sos/orsapi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<page>/<action>',views.action),
-    path('<page>/<action>/',views.action),
-    path('<page>/<action>/<int:id>',views.action),
-]
